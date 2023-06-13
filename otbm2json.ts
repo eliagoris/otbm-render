f8a4b95925d64b091b3c1579d54097276b5ca583
ellagoris <eduardofidera@gmail.com>
2023-06-13 19:06:31 -0300

    update structure and scripts

---
+++ otbm2json.ts
@@ -0,0 +1,7 @@
+import { writeFileSync } from "fs"
+
+// @ts-ignore
+import { read, serialize, write } from "otbm2json"
+const data = read("./public/Untitled.otbm")
+
+writeFileSync("Untitled.json", JSON.stringify(data))
